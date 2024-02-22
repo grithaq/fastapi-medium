430dd1fd1976934912c4b217f2dde9d49f8b4253
grithaq <afifnurazizi@gmail.com>
2024-02-23 04:58:18 +0800

    modify: delete debuging code

--- app/repositories.py
+++ app/repositories.py
@@ -35,9 +35,6 @@ class UserRepository:
         return self.users
     
     def add(self, user):
-        print("user in repo", user)
-        print(user["id"])
-        print(user["name"])
         user_model = User()
         user_model.id = user["id"]
         user_model.name = user["name"]
