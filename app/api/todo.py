3150644452c8c128668f1f5ed8233fc2bd67ac43
grithaq <afifnurazizi@gmail.com>
2024-02-23 10:46:27 +0800

    update: tidy up router sintax

--- app/api/todo.py
+++ app/api/todo.py
@@ -5,6 +5,8 @@ from schema import TodoSchema
 router = APIRouter()
 
 
-@router.post("/todo", status_code=status.HTTP_201_CREATED, tags=['TODO'])
+@router.post(
+        "/todo", status_code=status.HTTP_201_CREATED, tags=['TODO']
+)
 def create_todo(todo: TodoSchema):
     print(todo)
