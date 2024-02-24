4a0ec5bdb6a938bb63cd3439d61354481ca20352
grithaq <afifnurazizi@gmail.com>
2024-02-24 15:23:52 +0800

    feature: create configuration settings

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, APIRouter
 from api import todo, user, category
+from core.config import settings
 
 app = FastAPI(
-    title="Grithaq TODO API",
-    description="API for Grithaq TODO App",
-    version="1.0.0",
+    title=settings.PROJECT_NAME,
+    description=settings.DESCRIPTION,
+    version=settings.PROJECT_VERSION,
+    debug=settings.DEBUG
 )
 
 router = APIRouter()
 
-app.include_router(todo.router, prefix="/api/v1")
-app.include_router(user.router, prefix="/api/v1")
-app.include_router(category.router, prefix="/api/v1")
+app.include_router(todo.router, prefix=settings.API_V1_STR)
+app.include_router(user.router, prefix=settings.API_V1_STR)
+app.include_router(category.router, prefix=settings.API_V1_STR)
