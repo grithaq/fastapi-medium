ed7c94d20271c6f695300918a90212767d3bd9fa
grithaq <afifnurazizi@gmail.com>
2024-02-28 09:16:57 +0800

    fix: fix error and create error response

--- app/core/error.py
+++ app/core/error.py
@@ -1,4 +1,10 @@
 class NewError(Exception):
-    
-    def __init__(self):
-        return "New error"
+    status: str
+    msg: str
+
+    def __init__(self, status: str, msg: str) -> None:
+        self.status = status
+        self.msg = msg
+
+    def __repr__(self) -> str:
+        return f"status: {self.status} , message: {self.msg}"
