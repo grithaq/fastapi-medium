76e89eb2bdd3c33d4ff24cc79d863a6b312d5a9e
grithaq <afifnurazizi@gmail.com>
2024-03-02 21:01:16 +0800

    fix: fix erorr

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from schema import SignUpSchema, SignUpResponse, SignInResponse
-from utils import has_pass, authenticate_user, create_access_token
+from utils import has_pass, authenticate_user, create_access_token, get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
 from core.config import settings
-import repositories
 from datetime import timedelta
+from typing import Annotated
+import repositories
 
 router = APIRouter()
 
@@ -28,8 +30,10 @@ def sign_up(account: SignUpSchema):
 @router.post(
     "/sign_in", tags=['Auth'], status_code=status.HTTP_200_OK
 )
-def login_for_access_token(username: str, passwword: str):
-    user = authenticate_user(repositories.db_users.get(), username, passwword)
+def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(repositories.db_users.get(), form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -43,3 +47,13 @@ def login_for_access_token(username: str, passwword: str):
     return SignInResponse(
         access_token=access_token, token_type="bearer"
     )
+
+
+@router.get(
+    '/account/me', tags=['Auth'], status_code=status.HTTP_200_OK,
+    response_model=SignUpSchema
+)
+def get_me(
+    current_user: Annotated[SignUpSchema, Depends(get_current_user)]
+):
+    return current_user
