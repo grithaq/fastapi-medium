d3b2863896c6c9b53ecbe1fc096863443759a2c9
grithaq <afifnurazizi@gmail.com>
2024-03-05 21:09:56 +0800

    feat: test sign_up endpoint

--- tests/test_sign_up.py
+++ tests/test_sign_up.py
@@ -1,2 +1,10 @@
-def test_lagi():
-    assert 1==1
+import httpx
+
+def test_sign_up():
+    r = httpx.post(url="http://localhost:8000/api/v1/sign_up",json={"id": "1","username": "grithaq","email": "gritmail","password": "asdf"},
+    headers={
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    })
+
+    assert r.status_code == 200
