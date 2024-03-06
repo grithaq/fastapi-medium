d77f637bd4997a52813f1afa9d130a9652c9e0e4
grithaq <afifnurazizi@gmail.com>
2024-03-06 22:28:59 +0800

    feat: create categories end to end test

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -1,19 +1,66 @@
-from app.core.config import settings
 from fastapi.testclient import TestClient
-from tests.conftest import get_token
+
+from app.core.config import settings
 
 
-
-def test_get_categories(client: TestClient, get_token):    
+def test_get_category(client: TestClient, get_token):
     response = client.get(
-        url=f"{settings.API_V1_STR}/categories",
-        params={
-            "page": 1,
-            "per_page": 10
-        },
-        headers={
-            'accept': "application/json",
-            "Authorization": get_token
-        }
+        url=f"{settings.API_V1_STR}/category",
+        params={"page": 1, "per_page": 10},
+        headers={"accept": "application/json", "Authorization": get_token},
     )
-    response.status_code == 200
+    assert response.status_code == 200
+    assert response.json() != {"detail": "Could not Validate"}
+    assert response.json() == {
+        "message": "Success",
+        "status": "200",
+        "data": [],
+        "user_id": 1,
+        "pagination": {"current": 1, "total": 0},
+    }
+
+
+def test_createa_category(client: TestClient, get_token):
+    category_data = {"id": 1, "name": "Learning"}
+    response = client.post(
+        url=f"{settings.API_V1_STR}/category",
+        headers={"accept": "application/json", "Authorization": get_token, "Content-Type": "application/json"},
+        json=category_data,
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        "message": "Success",
+        "status": "201",
+        "data": [{"id": 1, "name": "Learning"}],
+        "user_id": 1,
+    }
+
+
+def test_update_category(client: TestClient, get_token):
+    category_data = {"id": 1, "name": "Shoping"}
+    response = client.put(
+        url=f"{settings.API_V1_STR}/category/1",
+        headers={"accept": "application/json", "Authorization": get_token, "Content-Type": "application/json"},
+        json=category_data,
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Success",
+        "status": "200",
+        "data": [{"id": 1, "name": "Shoping"}],
+        "user_id": 1,
+    }
+
+
+def test_delete_category(client: TestClient, get_token):
+    response = client.delete(
+        url=f"{settings.API_V1_STR}/category/1",
+        headers={"accept": "application/json", "Authorization": get_token, "Content-Type": "application/json"},
+    )
+    assert response.status_code == 404
+    assert response.json() == {
+        "message": "Success",
+        "status": "200",
+        "data": [],
+        "user_id": 1,
+    }
